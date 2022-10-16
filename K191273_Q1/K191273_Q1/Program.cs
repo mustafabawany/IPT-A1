d9758fb081604c50bf3bd21126bd6fb19748c6e7
Muhammad Mustafa Bawany <63657424+mustafabawany@users.noreply.github.com>
2022-10-17 00:09:51 +0500

    Initial Commit

---
+++ K191273_Q1/K191273_Q1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace K191273_Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String websiteLink = args[0];
+            String dirPath = args[1];
+            WebClient client = new WebClient();
+            try 
+            {
+                client.DownloadFile(websiteLink, @dirPath + "\\index.html");
+                Console.WriteLine(dirPath + " - will contain a file named index.html");
+            }
+            catch (System.Net.WebException)
+            {
+                Console.WriteLine("Directory Not Found");
+            }
+            client.Dispose();
+        }
+    }
+}
